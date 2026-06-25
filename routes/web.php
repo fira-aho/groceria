1d9cf722c3fea3b7354d4095553c869cef40e920
Rafi Adriyan <rafiiadriyan@gmail.com>
2026-06-25 15:45:25 +0700

    merapihkan route

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,32 @@
 <?php
 
 use App\Http\Controllers\CartController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Di sini Anda dapat mendaftarkan rute web untuk aplikasi Anda.
+|
+*/
+
+// Rute Halaman Utama
 Route::get('/', [HomeController::class, 'index']);
 
-use App\Http\Controllers\CheckoutController;
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/checkout', [CheckoutController::class, 'index']);
-Route::post('/checkout', [CheckoutController::class, 'store']);
-
-Route::view('/success', 'success');
-
-// Menampilkan halaman utama keranjang belanja
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-
-// Menangani request AJAX untuk update kuantitas barang
-Route::post('/cart/update-qty', [CartController::class, 'updateQty'])->name('cart.update_qty');
-
-
-// Menampilkan form register
+// Rute untuk Registrasi
 Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
-
-// Memproses data register
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
+// Rute untuk Keranjang Belanja (Cart)
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/update-qty', [CartController::class, 'updateQty'])->name('cart.update_qty');
+
+// Rute untuk Checkout
+Route::get('/checkout', [CheckoutController::class, 'index']);
+Route::post('/checkout', [CheckoutController::class, 'store']);
+Route::view('/success', 'success');
