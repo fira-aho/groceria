bc934462d92e5e7471bd8bbb9d8b2be568722be1
Rafi Adriyan <rafiiadriyan@gmail.com>
2026-07-10 10:00:06 +0700

    seeder fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             AdminSeeder::class,
             ProductsRecomendationSeeder::class,
+            ProductCategorySeeder::class,
         ]);
     }
 }
