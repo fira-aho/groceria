7ee0ca72e1a6d0c211b93b4989c33a3c35a74672
Rafi Adriyan <rafiiadriyan@gmail.com>
2026-07-10 10:30:04 +0700

    /,,

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
         $this->call([
             ProductSeeder::class,
-            AdminSeeder::class,
             ProductsRecomendationSeeder::class,
             ProductCategorySeeder::class,
         ]);
